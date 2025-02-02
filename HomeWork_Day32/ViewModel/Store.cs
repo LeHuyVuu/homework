c6aa34d7d4b9f34f86598abeec89af155c208517
LeHuyVuu <lehuyvuok@gmail.com>
2025-02-02 13:36:38 +0700

    32

---
+++ HomeWork_Day32/ViewModel/Store.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_Day33.ViewModel;
+
+public class Store
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+}
