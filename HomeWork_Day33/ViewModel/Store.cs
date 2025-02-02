7df1e7c46644598a8e774e14016e920ca77e6d21
LeHuyVuu <lehuyvuok@gmail.com>
2025-02-02 13:34:38 +0700

    day 33

--- HomeWork_Day33/ViewModel/Store.cs
+++ HomeWork_Day33/ViewModel/Store.cs
@@ -1,6 +1,10 @@
-﻿namespace DefaultNamespace;
+﻿namespace HomeWork_Day33.ViewModel;
 
 public class Store
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
 }
